62fc772b54c773fbf648c4fa1bf3a0f66468d2f6
Peter McKay <peteramckay@gmail.com>
2015-04-19 22:42:51 -0700

    adding nginx files

---
+++ beams-node.js
@@ -0,0 +1,19 @@
+/*
+
+Beams v0.1.3 (Beta)
+Copyright (c) 2014-2015 Peter McKay
+Free to use under the MIT license.
+
+*/
+
+var express = require('express');
+var app = express();
+var mongo = require('mongoskin');
+var Db = mongo.Db;
+var oneDay = 86400000; 
+
+app.use(express.static(__dirname + '/site', { maxAge: oneDay })); // Serve up site content...
+
+app.listen(process.env.PORT || 80);
+
+console.log('Eureka! Server is running at port 80.');
